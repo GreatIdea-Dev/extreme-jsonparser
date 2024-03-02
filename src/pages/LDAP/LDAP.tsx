885153ef0677d513af52b0ed3ee3fe5fa0bbd3b1
John Larson <jl@buildmidwestern.com>
2024-03-02 17:17:06 -0600

    feat: added json upload for LDAP

--- src/pages/LDAP/LDAP.tsx
+++ src/pages/LDAP/LDAP.tsx
@@ -1,7 +1,283 @@
+import { JSX, createSignal } from "solid-js";
+
+export type EntryData = {
+  "openconfig-lldp:interfaces": {
+    interface: TableData[];
+  };
+};
+
+export type NeighborType = {
+  state: {
+    "chassis-id": string;
+
+    "system-description": string;
+
+    age: number;
+
+    "management-address": string;
+
+    "port-id-type": string;
+
+    "port-id": number;
+
+    "chassis-id-type": string;
+
+    "last-update": number;
+
+    "port-description": string;
+
+    id: string;
+
+    "system-name": string;
+  };
+
+  id: string;
+};
+
+export type CountersType = {
+  "tlv-discard": number;
+
+  "tlv-unknown": number;
+
+  "frame-error-in": number;
+
+  "last-clear": string;
+
+  "frame-discard": number;
+
+  "frame-out": number;
+
+  "frame-error-out": number;
+
+  "frame-in": number;
+};
+
+export type TableData = {
+  neighbors?: {
+    neighbor: NeighborType[];
+  };
+
+  state?: {
+    enabled: boolean;
+
+    name: string;
+
+    counters: CountersType;
+  };
+
+  config?: {
+    enabled: boolean;
+
+    name: string;
+  };
+
+  name?: string;
+};
+
 export default function LDAP() {
+  const [tableData, setTableData] = createSignal<JSX.Element>();
+  const [entryData, setEntryData] = createSignal<TableData[]>([]);
+  const [neighborsData, setNeighborsData] = createSignal();
+  const [openModal, setOpenModal] = createSignal(false);
+
+  const handleFilter = () => {
+    const filterType = (
+      document.querySelector("#filterDropdown") as HTMLSelectElement
+    ).value;
+    const filterValue = (
+      document.querySelector("#filterInput") as HTMLSelectElement
+    ).value
+      .toLowerCase()
+      .trim();
+    switch (filterType) {
+      default:
+        break;
+    }
+  };
+
+  const handleEntryData = () => {
+    const data = JSON.parse(
+      (document.querySelector("#jsonEntry") as HTMLInputElement).value
+    ) as EntryData;
+
+    console.log(data);
+
+    const newEntries = data["openconfig-lldp:interfaces"].interface;
+
+    setEntryData(newEntries);
+
+    setTableData(
+      newEntries.map((entry) => {
+        return (
+          <tr>
+            <td>{entry.name}</td>
+            <td>{entry.config?.name}</td>
+            <td>{entry.config?.enabled ? "true" : "false"}</td>
+            <td>{entry.state?.name}</td>
+            <td>{entry.state?.enabled ? "true" : "false"}</td>
+            <td
+              onClick={() =>
+                entry.neighbors && handleModalData(entry.neighbors.neighbor)
+              }
+              class={
+                entry.neighbors
+                  ? `cursor-pointer text-purple-500 italic hover:underline`
+                  : `text-gray-500 italic`
+              }
+            >
+              {entry.neighbors ? "View Neighbors" : "No Neighbors"}
+            </td>
+          </tr>
+        ) as JSX.Element;
+      })
+    );
+  };
+
+  const handleModalData = (neighbors: NeighborType[]) => {
+    console.log(neighbors);
+    setNeighborsData(
+      neighbors.map((neighbor) => {
+        return (
+          <tr>
+            <td>{neighbor.state["chassis-id"]}</td>
+            <td>{neighbor.state["system-description"]}</td>
+            <td>{neighbor.state.age}</td>
+            <td>{neighbor.state["management-address"]}</td>
+            <td>{neighbor.state["port-id-type"]}</td>
+            <td>{neighbor.state["port-id"]}</td>
+            <td>{neighbor.state["chassis-id-type"]}</td>
+            <td>{neighbor.state["last-update"]}</td>
+            <td>{neighbor.state["port-description"]}</td>
+            <td>{neighbor.state.id}</td>
+            <td>{neighbor.state["system-name"]}</td>
+          </tr>
+        ) as JSX.Element;
+      })
+    );
+    setOpenModal(true);
+  };
+
   return (
-    <div>
-      <h1>LDAP API JSON Parser</h1>
-    </div>
+    <>
+      <div
+        class={
+          openModal()
+            ? `modal fixed w-full h-full top-0 left-0 flex items-center justify-center`
+            : `hidden`
+        }
+      >
+        <div class="modal-container bg-neutral-200 dark:bg-neutral-900 w-screen h-screen overflow-x-scroll overflow-y-scroll rounded shadow-lg p-16">
+          <div class="modal-content text-left p-6 flex flex-col justify-start items-center overflow-hidden">
+            <div class="flex justify-between items-center pb-3 w-full">
+              <p class="text-2xl font-bold">Neighbor Data</p>
+              <div class="flex justify-end w-56">
+                <button class="apiButton" onClick={() => setOpenModal(false)}>
+                  Close
+                </button>
+              </div>
+            </div>
+
+            <div class="w-full h-full overflow-scroll">
+              <table class="w-auto h-auto">
+                <thead>
+                  <tr>
+                    <th>Chassis ID</th>
+                    <th class="px-32">System Description</th>
+                    <th>Age</th>
+                    <th>Management Address</th>
+                    <th>Port ID Type</th>
+                    <th>Port ID</th>
+                    <th>Chassis ID Type</th>
+                    <th>Last Update</th>
+                    <th>Port Description</th>
+                    <th>ID</th>
+                    <th>System Name</th>
+                  </tr>
+                </thead>
+                <tbody>{neighborsData as unknown as JSX.Element}</tbody>
+              </table>
+            </div>
+
+            <div class="flex justify-end pt-2 w-56">
+              <button class="apiButton" onClick={() => setOpenModal(false)}>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class={
+          openModal()
+            ? `hidden`
+            : `flex flex-col justify-start items-center w-screen h-screen px-4 sm:px-8 md:px-16`
+        }
+      >
+        <div class="w-full h-auto my-4 flex flex-col justify-center items-center">
+          <h1>Netwwork Instance API JSON Parser</h1>
+          <p class="text-xl">
+            Paste Data as JSON from Postman to update table.
+          </p>
+        </div>
+        <div class="flex flex-row justify-center items-center w-full">
+          <div class="flex w-full justify-start items-center flex-col">
+            <div class="apiForm">
+              <div class="flex flex-col justify-center items-start gap-2 w-full">
+                <textarea
+                  class="apiFormInput"
+                  id="jsonEntry"
+                  placeholder={"Paste JSON Here"}
+                />
+              </div>
+            </div>
+            <div class="w-full flex flex-row justify-start items-center gap-3">
+              <div class="w-full my-4 flex flex-row justify-start items-center">
+                <button class="apiButton" onClick={handleEntryData}>
+                  Update Table Data
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-full flex flex-col justify-center items-start gap-4 py-4">
+          <div class="flex flex-row flex-start items-center w-full">
+            <div class="flex flex-row gap-3 justify-start items-center">
+              <div class="filter">
+                <select id="filterDropdown" class="filterInput">
+                  <option value="mac">Name</option>
+                  <option value="vlan">Config Name</option>
+                  <option value="entryType">Config Enabled?</option>
+                  <option value="interface">State Name</option>
+                  <option value="subinterface">State Enabled?</option>
+                </select>
+                <textarea
+                  class="filterInput"
+                  id="filterInput"
+                  placeholder="Enter Value"
+                />
+                <button class="filterButton" onClick={handleFilter}>
+                  Filter
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="w-full overflow-x-scroll flex flex-row justify-center items-center">
+            <table class="w-auto  min-w-full">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Config Name</th>
+                  <th>Config Enabled?</th>
+                  <th>State Name</th>
+                  <th>State Enabled?</th>
+                  <th>Neighbors</th>
+                </tr>
+              </thead>
+              <tbody>{tableData as unknown as JSX.Element}</tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
